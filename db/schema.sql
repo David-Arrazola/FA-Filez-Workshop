63543e18c90fe73f2c9e5bf67d82c5776bfe2afa
David Arrazola <arrazola.dvd@gmail.com>
2026-02-23 19:53:06 -0500

    CREATE a new database, filez. CREATE schema for tables in DB filez. COMPLETE seed.js file, so that tables are seeded with fake data

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,17 @@
--- TODO
+DROP TABLE IF EXISTS folders CASCADE;
+DROP TABLE IF EXISTS files;
+
+CREATE TABLE folders(
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL UNIQUE
+);
+
+CREATE TABLE files(
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    size INT NOT NULL,
+    folder_id INT NOT NULL REFERENCES folders(id) ON DELETE CASCADE,
+    UNIQUE(name, folder_id) 
+);
+--* "UNIQUE(a, c)" specifies that the combination of values in the indicated columns is 
+--* unique across the whole table
--- db/seed.js
+++ db/seed.js
@@ -1,4 +1,5 @@
 import db from "#db/client";
+import { faker } from "@faker-js/faker";
 
 await db.connect();
 await seed();
@@ -6,5 +7,31 @@ await db.end();
 console.log("🌱 Database seeded.");
 
 async function seed() {
-  // TODO
+  try {
+    const folderSql = `
+      INSERT INTO folders (name)
+      VALUES ($1)
+      RETURNING id;
+    `;
+
+    const fileSql = `
+      INSERT INTO files (name, size, folder_id)
+      VALUES ($1, $2, $3)
+    `;
+
+    for (let i = 0; i < 3; i++) {
+      const folderName = faker.word.noun();
+      const { rows } = await db.query(folderSql, [folderName]);
+      const folderId = rows[0].id;
+
+      for (let j = 0; j < 5; j++) {
+        const fileName = faker.system.fileName();
+        const fileSize = faker.number.int({ min: 1, max: 100000 });
+
+        await db.query(fileSql, [fileName, fileSize, folderId]);
+      }
+    }
+  } catch (e) {
+    console.error(e);
+  }
 }
